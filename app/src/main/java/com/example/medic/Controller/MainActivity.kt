953e70a1a176d9194cc95a5bf6959c97d534e01d
Berk Er <berkerr.tr@gmail.com>
2021-12-16 16:26:11 +0300

    Main Activity
    Main Activity started to design

--- app/src/main/java/com/example/medic/Controller/MainActivity.kt
+++ app/src/main/java/com/example/medic/Controller/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.medic.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.medic.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
