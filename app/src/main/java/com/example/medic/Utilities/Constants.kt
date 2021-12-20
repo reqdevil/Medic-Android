d8ff0662fdbd4577d2feb633fbb840f0481dcfa8
Berk Er <berkerr.tr@gmail.com>
2021-12-21 00:12:25 +0300

    Create Constants.kt

---
+++ app/src/main/java/com/example/medic/Utilities/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.medic.Utilities
+
+val DATA_UPDATED: String = "Data Update"
+val ERROR_OCCURED: String = "Bir hata oluştu"
+val FILL_BLANKS: String = "Lütfen bütün alanları doldurunuz"
