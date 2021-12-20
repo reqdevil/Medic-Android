cc16cce81265ba563f930edc43ccecc14b87fdb4
Berk Er <berkerr.tr@gmail.com>
2021-12-21 00:11:10 +0300

    Update LoginActivity.kt

--- app/src/main/java/com/example/medic/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/medic/Controller/LoginActivity.kt
@@ -1,8 +1,9 @@
 package com.example.medic.Controller
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Color
-import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -24,19 +25,29 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var signUpButton: Button
     private lateinit var signUpView: LinearLayout
     private lateinit var forgotPassword: TextView
+    private lateinit var pref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        pref = getPreferences(Context.MODE_PRIVATE)
+        val username = pref.getString("username", "")
+        val password = pref.getString("password", "")
+
         usernameField = findViewById(R.id.usernameField)
         passwordField = findViewById(R.id.passwordField)
+        if (username != null && password != null) {
+            usernameField.setText(username)
+            passwordField.setText(password)
+        }
+
         nameField = findViewById(R.id.nameField)
         surnameField = findViewById(R.id.surnameField)
         phoneField = findViewById(R.id.phoneField)
         mailField = findViewById(R.id.mailField)
 
-        signInButton = findViewById(R.id.signin)
+        signInButton = findViewById(R.id.updateButton)
         signUpButton = findViewById(R.id.signup)
 
         signUpView = findViewById(R.id.signUpView)
@@ -55,10 +66,18 @@ class LoginActivity : AppCompatActivity() {
         signUpButton.setOnClickListener() {
             if (signUpView.visibility == View.GONE) {
                 signUpView.visibility = View.VISIBLE
+                usernameField.setText("")
+                passwordField.setText("")
             } else {
-                val userInfo: HashMap<String, Any> = hashMapOf("email" to mailField.text.toString(), "username" to usernameField.text.toString(), "password" to passwordField.text.toString(), "name" to nameField.text.toString(), "surname" to surnameField.text.toString(), "phoneNumber" to phoneField.text.toString())
+                val userInfo: HashMap<String, Any> = hashMapOf("isDoctor" to false)
+                userInfo["email"] = mailField.text.toString()
+                userInfo["username"] = usernameField.text.toString()
+                userInfo["password"] = passwordField.text.toString()
+                userInfo["name"] = nameField.text.toString().replaceFirstChar { it.uppercase() }
+                userInfo["surname"] = surnameField.text.toString().replaceFirstChar { it.uppercase() }
+                userInfo["phoneNumber"] = phoneField.text.toString()
 
-                signUp(mailField.text.toString(), passwordField.text.toString(), userInfo)
+                signUp(usernameField.text.toString(), mailField.text.toString(), passwordField.text.toString(), userInfo)
             }
         }
 
@@ -66,10 +85,13 @@ class LoginActivity : AppCompatActivity() {
             val forgotPassword = Intent(this, ForgotPasswordActivity::class.java)
             startActivity(forgotPassword)
         }
+
+        phoneField.setOnClickListener() {
+            phoneField.setText("")
+        }
     }
 
     private fun signIn(username: String, password: String) {
-        Log.i(TAG, "User entrance started")
         signInButton.setBackgroundColor(Color.rgb(57, 88, 119))
         signInButton.text = "Giriş Yapılıyor"
 
@@ -78,8 +100,7 @@ class LoginActivity : AppCompatActivity() {
                 signInButton.setBackgroundColor(Color.rgb(87, 138, 167))
                 signInButton.text = "Giriş Yapıldı"
 
-                val mainActivity = Intent(this, MainActivity::class.java)
-                startActivity(mainActivity)
+                enterApplication(username, password)
             } else {
                 signInButton.setBackgroundColor(Color.rgb(206, 22, 20))
                 signInButton.text = "Giriş Yap"
@@ -89,25 +110,33 @@ class LoginActivity : AppCompatActivity() {
         })
     }
 
-    private fun signUp(username: String, password: String, userInfo: HashMap<String, Any>) {
-        Log.i(TAG, "User creation started")
+    private fun signUp(username: String, email: String, password: String, userInfo: HashMap<String, Any>) {
         signUpButton.setBackgroundColor(Color.rgb(57, 88, 119))
         signUpButton.text = "Kayıt Yapılıyor"
+        Log.e(TAG, email)
 
-        AuthenticationService.instance.signUpUser(username, password, userInfo, completion = { success, error ->
+        AuthenticationService.instance.signUpUser(email, password, userInfo, completion = { success, error ->
             if (success) {
                 signUpButton.setBackgroundColor(Color.rgb(87, 138, 167))
                 signUpButton.text = "Kayıt Yapıldı"
 
-                val mainActivity = Intent(this, MainActivity::class.java)
-                startActivity(mainActivity)
+                enterApplication(username, password)
             } else {
                 signUpButton.setBackgroundColor(Color.rgb(206, 22, 20))
                 signUpButton.text = "Kayıt Ol"
 
                 Toast.makeText(this, error?.localizedMessage, Toast.LENGTH_SHORT).show()
-                Log.e(TAG, error?.localizedMessage.toString())
             }
         })
+    }
+
+    private fun enterApplication(username: String, password: String) {
+        val editor = pref.edit()
+        editor.putString("username", username)
+        editor.putString("password", password)
+        editor.apply()
+
+        val mainActivity = Intent(this, MainActivity::class.java)
+        startActivity(mainActivity)
     }
 }
