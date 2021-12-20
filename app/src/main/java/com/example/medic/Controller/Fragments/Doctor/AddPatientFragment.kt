c4a91b53a4d44ad4297046531d2fe7326bfc19a2
Berk Er <berkerr.tr@gmail.com>
2021-12-21 00:11:58 +0300

    Hasta Ekle Recycler View
    Hasta eklemek için recyler view oluşturuldu. Adapter'ı yazıldı

--- app/src/main/java/com/example/medic/Controller/Fragments/Doctor/AddPatientFragment.kt
+++ app/src/main/java/com/example/medic/Controller/Fragments/Doctor/AddPatientFragment.kt
@@ -1,60 +1,68 @@
 package com.example.medic.Controller.Fragments.Doctor
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.medic.Interface.OnDataPass
+import com.example.medic.Model.Patient
 import com.example.medic.R
+import com.example.medic.Services.DatabaseService
+import com.example.medic.View.MyAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class AddPatientFragment : Fragment(), OnDataPass {
+    private val TAG = "Add Patient Fragment"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddPatientFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class AddPatientFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var patientView: RecyclerView
+    private lateinit var patientList: ArrayList<Patient>
+
+    private lateinit var searchPatient: Button
+    private lateinit var usernameField: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        patientList = ArrayList<Patient>()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_patient, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddPatientFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddPatientFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        patientView = view.findViewById(R.id.patientView)
+        patientView.layoutManager = LinearLayoutManager(activity)
+        val adapter = MyAdapter(patientList)
+        patientView.adapter = adapter
+
+        usernameField = view.findViewById(R.id.usernameField)
+
+        searchPatient = view.findViewById(R.id.searchPatientButton)
+        searchPatient.setOnClickListener() { searchPatient(usernameField.text.toString()) }
+    }
+
+    private fun searchPatient(uid: String) {
+        Log.e(TAG, "search patient started")
+
+        DatabaseService.instance.findPatient(uid, completion = { patientList ->
+            patientView.adapter = MyAdapter(patientList)
+        })
+
+        Log.e(TAG, patientList.toString())
+    }
+
+    override fun onDataPass(data: String) {
+        Log.e(TAG, data)
     }
 }
