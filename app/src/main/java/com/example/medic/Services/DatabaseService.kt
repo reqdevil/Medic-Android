c1a8b9053beaef9b584b8e05dcc3739480790d86
Berk Er <berkerr.tr@gmail.com>
2021-12-21 00:12:47 +0300

    Update DatabaseService.kt

--- app/src/main/java/com/example/medic/Services/DatabaseService.kt
+++ app/src/main/java/com/example/medic/Services/DatabaseService.kt
@@ -1,19 +1,19 @@
 package com.example.medic.Services
 
 import android.util.Log
+import com.example.medic.Model.Patient
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
-import java.lang.Error
 
 class DatabaseService {
-    private var TAG: String = "Database Service"
+    private val TAG: String = "Database Service"
 
-    private var database: DatabaseReference = Firebase.database.reference
+    private val database: DatabaseReference = Firebase.database.reference
 
-    private var baseReference = database
-    private var userReference = baseReference.child("users")
+    private val baseReference = database
+    private val userReference = baseReference.child("users")
+    private val medicationReference = userReference.child(AuthenticationService.instance.getUID()).child("medication")
 
     companion object {
         val instance = DatabaseService()
@@ -27,7 +27,7 @@ class DatabaseService {
         userReference.child(uid).updateChildren(userInfo)
     }
 
-    fun getUserInformation(username: String, password: String, completion: (Boolean, String?, Exception?) -> Unit) {
+    fun getLoginInformation(username: String, password: String, completion: (Boolean, String?, Exception?) -> Unit) {
         userReference.get().addOnSuccessListener { usersSnapshot ->
             for (user in usersSnapshot.children) {
                 val id = user.child("username").value.toString()
@@ -36,7 +36,7 @@ class DatabaseService {
                 val mail = user.child("email").value.toString()
 
                 if (id == username && pwd == password) {
-                    Log.e(TAG, "User Found")
+
                     completion(true, mail, null)
                     return@addOnSuccessListener
                 }
@@ -46,15 +46,73 @@ class DatabaseService {
         }
     }
 
-    fun checkUsername(username: String, completion: (Boolean) -> Unit) {
-        userReference.get().addOnSuccessListener { userSnapshot ->
-            for (user in userSnapshot.children) {
-                val id = user.child("username").value.toString()
+    fun getUserInformation(uid: String, completion: (String, String, String, String, String, Boolean) -> Unit) {
+        userReference.get().addOnSuccessListener { usersSnapshot ->
+            for (user in usersSnapshot.children) {
+                val userID = user.key.toString()
 
-                if (id == username) {
+                if (userID == uid) {
+                    val username = user.child("username").value.toString()
+                    val name = user.child("name").value.toString()
+                    val surname = user.child("surname").value.toString()
+                    val email = user.child("email").value.toString()
+                    val phoneNumber = "0" + user.child("phoneNumber").value.toString()
+                    val isDoctor = user.child("isDoctor").value
 
+                    completion(username, name, surname, email, phoneNumber, isDoctor as Boolean)
                 }
             }
         }
+    }
+
+    fun findPatient(username: String, completion: (ArrayList<Patient>) -> Unit) {
+        val patientList: ArrayList<Patient> = ArrayList()
+
+        userReference.get().addOnSuccessListener { usersSnapshot ->
+            for (user in usersSnapshot.children) {
+                val id = user.child("username").value.toString()
+                val isDoctor = user.child("isDoctor").value
+
+                if (id.contains(username) && isDoctor == false) {
+                    val name = user.child("name").value.toString()
+                    val surname = user.child("surname").value.toString()
+                    val uid = user.key.toString()
+
+                    patientList.add(Patient(uid, id, "$name $surname"))
+//                    StorageService.instance.getProfilePicture(AuthenticationService.instance.getUID(), completion = { success, byteArray, error ->
+//                        if (success) {
+//                            val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
+//
+//                        } else {
+//                            patientList.add(Patient(null, id, "$name $surname"))
+//                        }
+//                    })
+                }
+            }
+
+            completion(patientList)
+        }
+    }
+
+    fun updateMedication(uid: String, medicationInfo: HashMap<String, Any>) {
+        userReference.child(uid).child("medication").setValue(medicationInfo)
+    }
+
+    fun getMedication() {
+        medicationReference.get().addOnSuccessListener { medicationSnapshot ->
+            for (medication in medicationSnapshot.children) {
+                Log.e(TAG, medication.child("name").value.toString() + "|" + medication.child("period").value.toString())
+            }
+        }
+    }
+
+    fun addPatient(uid: String, patient: Patient) {
+        val userInfo = hashMapOf("name" to patient.fullName.split(" ")[0], "surname" to patient.fullName.split(" ")[1], "uid" to patient.uid)
+        userReference.child(uid).child("patients").child(patient.username).setValue(userInfo)
+
+        getUserInformation(uid, completion = { username, name, surname, _, _, _ ->
+            val userInfo = hashMapOf("name" to name, "surname" to surname, "uid" to uid)
+            userReference.child(patient.uid).child("doctors").child(username).setValue(userInfo)
+        })
     }
 }
