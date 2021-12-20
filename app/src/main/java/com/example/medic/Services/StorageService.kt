6b5bcacbcc86f2a0dd68151653261152b88df22f
Berk Er <berkerr.tr@gmail.com>
2021-12-21 00:12:41 +0300

    Create StorageService.kt
    Storage servisi oluşturuldu. Profil fotoğrafı yükleme ve indirme servisleri yazıldı

---
+++ app/src/main/java/com/example/medic/Services/StorageService.kt
@@ -0,0 +1,33 @@
+package com.example.medic.Services
+
+import android.net.Uri
+import android.util.Log
+import androidx.core.net.toFile
+import com.google.firebase.storage.FirebaseStorage
+import java.io.File
+import java.lang.Exception
+
+class StorageService {
+    private val TAG = "Storage Service"
+
+    private val reference = FirebaseStorage.getInstance().reference
+    private val profileReference = reference.child("profilePictures")
+
+    companion object {
+        val instance = StorageService()
+    }
+
+    fun uploadProfilePicture(uid: String, uri: Uri) {
+        profileReference.child(uid).putFile(uri)
+    }
+
+    fun getProfilePicture(uid: String, completion: (Boolean, ByteArray?, Exception?) -> Unit) {
+        profileReference.child(uid).getBytes(1024 * 1024).addOnCompleteListener() { task ->
+            if (task.isSuccessful) {
+                completion(true, task.result, null)
+            } else {
+                completion(false, null, task.exception)
+            }
+        }
+    }
+}
