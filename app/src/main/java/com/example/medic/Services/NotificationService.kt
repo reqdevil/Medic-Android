891f36dd9966cea61d781fbad22eba100ff0ebea
Berk Er <berkerr.tr@gmail.com>
2021-12-22 15:03:04 +0300

    Create NotificationService.kt
    created notification service to create/remove local notification.

---
+++ app/src/main/java/com/example/medic/Services/NotificationService.kt
@@ -0,0 +1,28 @@
+package com.example.medic.Services
+
+import android.util.Log
+
+class NotificationService {
+    private val TAG = "Notification Service"
+
+    companion object {
+        val instance = NotificationService()
+    }
+
+    private fun decider(name: String) {
+        /*
+        if name exist
+            removeAlarm()
+        else
+            createAlarm()
+         */
+    }
+
+    private fun createAlarm() {
+
+    }
+
+    private fun removeAlarm() {
+
+    }
+}
